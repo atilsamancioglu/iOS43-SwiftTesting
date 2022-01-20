f9874cb45fd7085cd7c469c7d208899544849af7
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-01-20 11:02:52 +0300

    MathematicFunctions class created, Test files cleared

---
+++ SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,24 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Atil Samancioglu on 20.01.2022.
+//
+
+import Foundation
+
+class MathematicFunctions {
+    
+    func addIntegers(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyNumbers(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func divideIntegers(x: Int, y: Int) -> Int {
+        return x / y
+    }
+    
+}

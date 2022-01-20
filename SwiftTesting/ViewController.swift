f3bd389247a488f8e601aae9e37d7f2c94b58c32
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-01-20 12:24:59 +0300

    To-do list UI added for UI Tests

--- SwiftTesting/ViewController.swift
+++ SwiftTesting/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    
+    var toDoList = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
     }
 
 
+    @IBAction func addClicked(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Add Item", message: "Enter Your To Do Item", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Enter Item"
+        }
+        let okButton = UIAlertAction(title: "OK", style: .default) { action in
+            
+            guard let textField = alert.textFields?[0], let inputText = textField.text, !inputText.isEmpty else {
+                return
+            }
+            self.toDoList.insert(inputText, at: 0)
+            self.tableView.insertRows(at: [.init(row: 0, section: 0)], with: .automatic)
+        }
+        
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return toDoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = toDoList[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.toDoList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
 }
 
--- SwiftTestingTests/SwiftTestingTests.swift
+++ SwiftTestingTests/SwiftTestingTests.swift
@@ -9,7 +9,29 @@ import XCTest
 @testable import SwiftTesting
 
 class SwiftTestingTests: XCTestCase {
-
     
+    let math = MathematicFunctions()
+
+
+    func testAddFunction() {
+        
+        let result = math.addIntegers(x: 5, y: 8)
+        
+        XCTAssertEqual(result, 13)
+    }
+    
+    func testMultiplyFunction() {
+        
+        let result = math.multiplyNumbers(x: 5, y: 8)
+        
+        XCTAssertEqual(result, 40)
+    }
+    
+    func testDivideFunction() {
+        
+        let result = math.divideIntegers(x: 10, y: 2)
+        
+        XCTAssertEqual(result, 5)
+    }
 
 }

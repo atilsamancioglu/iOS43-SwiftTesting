6342fa98f7ec3dbecd0e4bfdd2f43e04699bda36
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2022-01-20 14:27:11 +0300

    UI tests are added

--- SwiftTestingUITests/SwiftTestingUITests.swift
+++ SwiftTestingUITests/SwiftTestingUITests.swift
@@ -32,5 +32,38 @@ class SwiftTestingUITests: XCTestCase {
         
 
     }
+    
+    func testDeleteItem() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        
+        let addButton = app.navigationBars["SwiftTesting.View"].buttons["Add"]
+        
+        let textField =  app.alerts["Add Item"].scrollViews.otherElements.collectionViews.textFields["Enter Item"]
+        
+        let okBUtton = app.alerts["Add Item"].scrollViews.otherElements.buttons["OK"]
+        
+        let addedCell = app.tables.cells.containing(.staticText, identifier:"my to do").element
+        
+        
+        //tables
+        
+        let tablesQuery = app.tables
+        let deleteButton = tablesQuery.buttons["Delete"]
+
+        
+        addButton.tap()
+        textField.tap()
+        textField.typeText("my to do")
+        okBUtton.tap()
+        
+        addedCell.swipeLeft()
+        deleteButton.tap()
+        
+        XCTAssertFalse(addedCell.exists)
+                                           
+    
+    }
  
 }
